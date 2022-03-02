9d3ccc2df7d40135250d40d20c52e9fe0ec75506
Lucy Garcia <lucifer@Lucys-MacBook-Air.local>
2022-03-01 17:32:24 -0800

    automated db creation

--- module_11/createdb.py
+++ module_11/createdb.py
@@ -5,7 +5,7 @@
 
 
 
-#connecting to database
+#database config
 
 import mysql.connector
 
@@ -34,16 +34,24 @@ cursor.execute("DROP TABLE IF EXISTS user")
 #save to database
 db.commit()
 
-#Creating tables
+#Creating tables; store
 cursor.execute("CREATE TABLE store (store_id INT NOT NULL AUTO_INCREMENT, locale VARCHAR(500) NOT NULL, PRIMARY KEY(store_id))")
+
+#book
 cursor.execute("CREATE TABLE book (book_id INT NOT NULL AUTO_INCREMENT, book_name VARCHAR(200) NOT NULL, details VARCHAR(500), author VARCHAR(200) NOT NULL, PRIMARY KEY(book_id))")
+
+#user
 cursor.execute("CREATE TABLE user (user_id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(75) NOT NULL, last_name VARCHAR (75) NOT NULL, PRIMARY KEY(user_id))")
+
+#wishlist
 cursor.execute("CREATE TABLE wishlist (wishlist_id INT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, book_id INT NOT NULL, PRIMARY KEY(wishlist_id), FOREIGN KEY(user_id) REFERENCES user(user_id), FOREIGN KEY(book_id) REFERENCES book(book_id))")
 
 db.commit()
-#Insert data into tables
+
+#Insert data into store table
 cursor.execute("INSERT INTO store(store_id,locale) VALUES (1, '1400 Windy Street Oxnard, Ca 92700')")
 
+#Insert data into book table
 cursor.execute("INSERT INTO book(book_id, book_name, details, author) VALUES (1,'IT', 'Horror story about a killer clown', 'Stephen King')")
 cursor.execute("INSERT INTO book(book_id, book_name, details, author) VALUES (2, 'Odd Thomas', 'Story about an odd fellow', 'Stephen King')")
 cursor.execute("INSERT INTO book(book_id, book_name, details, author) VALUES (3, 'Clifford', 'A big red dog', 'Norman Bridwell')")
@@ -54,11 +62,14 @@ cursor.execute("INSERT INTO book(book_id, book_name, details, author) VALUES (7,
 cursor.execute("INSERT INTO book(book_id, book_name, details, author) VALUES (8, 'Misery', 'an author is rescued by an obsessive fan but the attraction is FATAL', 'Stephen King')")
 cursor.execute("INSERT INTO book(book_id, book_name, details, author) VALUES (9, 'The Girl Who Loved Tom Gordon', 'A little girl gets lost at a pit stop on her way to a ball game', 'Stephen King')")
 
-
+#Insert data into user table
 cursor.execute("INSERT INTO user(user_id,first_name,last_name) VALUES (1, 'Lucy', 'Garcia')")
+
 cursor.execute("INSERT INTO user(user_id,first_name,last_name) VALUES (2, 'Marley', 'Perez')")
+
 cursor.execute("INSERT INTO user(user_id,first_name,last_name) VALUES (3, 'Nancy', 'Perez')")
 
+#Insert data into wishlist table
 cursor.execute("INSERT INTO wishlist(wishlist_id,user_id,book_id) VALUES (1,1,6)")
 cursor.execute("INSERT INTO wishlist(wishlist_id,user_id,book_id) VALUES (2,2,4)")
 cursor.execute("INSERT INTO wishlist(wishlist_id,user_id,book_id) VALUES (3,3,5)")
